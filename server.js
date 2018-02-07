db5f3e1cea3547fc8e4acfac461bf8bb79c9cca7
Lynn Li <yuhua.li@inveno.com>
2018-02-07 10:33:58 +0800

    [bug]: replace 'server started at http://localhost:${port}' with `server started at http://localhost:${port}`

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('server started at http://localhost:${port}');
+    console.log(`server started at http://localhost:${port}`);
 });
