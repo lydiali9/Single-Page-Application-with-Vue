f5d3895d955bb29acd1e93c5936be67655740b53
Lynn Li <yuhua.li@inveno.com>
2018-02-07 10:51:14 +0800

    [feature]: add path relative the path from the path.resolve and add script to restart server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+// To make the path of the file more strict. and retrieve the file path fron the path.resolve method. it will return the path relative to the server.js file and from the directory that we run the script from.
+const path = require("path");
+
+// return the data of our index page.
+const indexHTML = (() => {
+    return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
+})();
 
 app.get('*', (req, res) => {
-    res.write('hey there');
+    res.write(indexHTML);
     res.end();
 });
 
