fee0f9ae02f8027e78c4a7e33d8f9f532a8ff851
Lynn Li <yuhua.li@inveno.com>
2018-02-08 11:12:04 +0800

    [feature]: accepts the file in which we want to save our styles in by ExtractTextPlugin

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,8 +1,17 @@
 const base = require("./webpack.base.config")
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // To include the base config, then will Extend the base config object by using Object.assign method and include the property named plugins.
 const config = Object.assign({}, base, {
     plugins: base.plugins || []
 })
+
+config.module.rules
+                .filter(x => { return x.loader == 'vue-loader' })
+                .forEach(x => x.options.extractCSS = true )
+
+config.plugins.push(
+    new ExtractTextPlugin('assets/styles.css')
+)
 
 module.exports = config
