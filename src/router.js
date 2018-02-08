990f9113ee0a4a39c283957d26467e5ebbda1e5a
Lynn Li <yuhua.li@inveno.com>
2018-02-08 13:52:36 +0800

    [feature]: add login page and the path of login

--- src/router.js
+++ src/router.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Category from './theme/Category.vue'
+import Login from './theme/Login.vue'
 
 // This way, the vue-router functionality is plugged into Vue.
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    routes: [{
-        path: '/',
-        component: Category
-    }]
+    mode: 'history', // If I visit the login path without the hash, everything loads as expected.
+    routes: [
+        { path: '/', component: Category },
+        { path: '/login', component: Login}
+    ]
 })
 
 export default router
